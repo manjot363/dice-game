3bd1fc582b3a8774383f486e9834dc0f086bc2c1
manjot363 <112740405+manjot363@users.noreply.github.com>
2023-05-16 10:23:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "dice" + randomDiceImage + ".png"; 
+var randomDiceSource = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
